5d53e56553a7b0259580b69a416922da4bae7663
Priscillia <selvapriscillia@yahoo.fr>
2024-12-16 20:48:09 +0100

    create page about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import Layout from "../ui/layout/layout";
 import Title from "../ui/title/title";
+import Image from "next/image";
 
 import type { Metadata } from "next";
 
@@ -12,7 +13,35 @@ export default function Page() {
   return (
     <Layout>
       <section className="pt-10 pb-10">
-        <Title title="À propos de l'auteur" />
+        <div className="flex justify-center items-center">
+          <Title title="À propos de l'auteur" />
+        </div>
+
+        <Image
+          src="/image.png"
+          alt="Frédéric DEPARIS"
+          width={300}
+          height={300}
+          className="w-screen h-auto"
+        />
+        <p className="p-2 text-justify ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+          nulla voluptates quia distinctio facilis dicta velit. Officia et
+          inventore quod laboriosam nisi necessitatibus, dolores aliquam hic
+          quasi dicta voluptates sed. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Impedit, nulla voluptates quia distinctio facilis
+          dicta velit. Officia et inventore quod laboriosam nisi necessitatibus,
+          dolores aliquam hic quasi dicta voluptates sed.
+        </p>
+        <p className="p-2 text-justify ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+          nulla voluptates quia distinctio facilis dicta velit. Officia et
+          inventore quod laboriosam nisi necessitatibus, dolores aliquam hic
+          quasi dicta voluptates sed. a voluptates quia distinctio facilis dicta
+          velit. Officia et inventore quod laboriosam nisi necessitatibus,
+          dolores aliquam hic quasi dicta voluptates sed. a voluptates quia
+          distinctio facilis dicta velit. Officia et inventore quod laboriosam
+        </p>
       </section>
     </Layout>
   );
