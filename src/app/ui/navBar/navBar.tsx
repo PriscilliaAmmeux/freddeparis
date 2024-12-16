c154b3ad58ac46a4d53a86bf1d251bd64e40e601
Priscillia <selvapriscillia@yahoo.fr>
2024-12-16 12:53:55 +0100

    create footer

--- src/app/ui/navBar/navBar.tsx
+++ src/app/ui/navBar/navBar.tsx
@@ -3,10 +3,10 @@ import SocialNetwork from "../socialNetwork/socialNetwork";
 
 export default function navBar() {
   return (
-    <section className="flex justify-between items-center p-4 w-full text-white bg-"> 
+    <section className="flex justify-between items-center p-4 w-full text-white bg-primary-color">
       <nav className="flex space-x-4">
         <NavLink href="/" title="Accueil" isActive={true} />
-        <NavLink href="/about" title="A propos de l'auteur" isActive={true} />
+        <NavLink href="/about" title="À propos de l'auteur" isActive={true} />
         <NavLink href="/contact" title="Contact" isActive={true} />
       </nav>
     </section>
