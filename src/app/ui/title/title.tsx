e927aef2e13ffd061aba3c4f315d84c4e2ab23c8
Priscillia <selvapriscillia@yahoo.fr>
2024-12-16 20:48:09 +0100

    create page about

--- src/app/ui/title/title.tsx
+++ src/app/ui/title/title.tsx
@@ -3,9 +3,10 @@ import styles from "./../../styles/title.module.css";
 
 interface TitleProps {
   title: string;
+  className?: string;
 }
 
-export default function Title({ title }: TitleProps) {
+export default function Title({ title, className }: TitleProps) {
   return (
     <div className="flex items-center mb-4">
       <h1 className={`text-xl font-bold ${styles.title}`}>{title}</h1>
