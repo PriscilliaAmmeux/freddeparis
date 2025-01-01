cc5ea352c9abd4ee1d253ff844bd979a3ddc41d0
Priscillia <selvapriscillia@yahoo.fr>
2025-01-01 23:54:38 +0100

    delete fb and insta social network

--- src/app/ui/socialNetwork/socialNetwork.tsx
+++ src/app/ui/socialNetwork/socialNetwork.tsx
@@ -6,23 +6,7 @@ export default function SocialNetwork() {
   return (
     <section className="pt-6 flex items-center gap-2">
       <a
-        href=""
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Link to Instagram profile"
-        className="cursor-pointer">
-        <AiOutlineInstagram size={24} className="text-current" />
-      </a>
-      <a
-        href=""
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Link to Facebook profile"
-        className="cursor-pointer">
-        <FiFacebook size={24} className="text-current" />
-      </a>
-      <a
-        href=""
+        href="https://www.linkedin.com/in/fr%C3%A9d%C3%A9ric-deparis-a2229664/"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Link to linkedIn profile"
