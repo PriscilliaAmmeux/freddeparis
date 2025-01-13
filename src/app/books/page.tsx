1a57853751cf10d9c8380c6cfd00e2646b3550fe
Priscillia <selvapriscillia@yahoo.fr>
2025-01-13 22:26:20 +0100

    add summary books, replace img book, design button

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -20,35 +20,45 @@ export default function Books() {
   return (
     <Layout>
       <section className="pt-10 pb-10 pl-2 pr-2">
-        <div className="flex items-center justify-center">
+        <div className="text-center mb-10">
           <Title title="Livres" />
+          <p className="text-lg text-gray-700 mt-4">
+            Deux trilogies pour le plaisir de lire
+          </p>
         </div>
-
         {trilogies.map((trilogy, index) => (
-          <div key={index} className="mb-10">
-            <h2 className="text-2xl font-bold">{trilogy.name}</h2>
-            <p className="mb-4 text-justify">{trilogy.summary}</p>
-            <div className="flex flex-wrap -mx-2">
+          <div key={index} className="mb-16">
+            <h2 className="text-xl font-bold text-center text-gray-800 mb-6">
+              {trilogy.name}
+            </h2>
+            <p className="text-lg text-gray-600 text-center mb-10">
+              {trilogy.summary}
+            </p>
+            <div className="flex flex-wrap justify-center -mx-4">
               {trilogy.books &&
                 trilogy.books.map((book) => (
-                  <div key={book.id} className="w-full md:w-1/2 px-2 mb-6">
-                    <div className="flex">
+                  <div key={book.id} className="w-full md:w-1/3 px-4 mb-8">
+                    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
                       <Image
                         src={book.image}
                         alt={book.title}
-                        width={200}
-                        height={100}
-                        className="w-full md:w-auto mr-0 md:mr-4 mb-4 md:mb-0 pr-2"
+                        width={150}
+                        height={225}
+                        className="w-full object-cover"
                       />
-                      <div className="w-3/4">
-                        <h3 className="text-xl font-semibold">{book.title}</h3>
-                        <p className="mb-2 text-justify">{book.summary}</p>
-                        <div className="flex gap-1">
-                          <Button
-                            type="button"
-                            text="Lire un extrait"
-                            className={styles["button-white-border"]}
-                          />
+                      <div className="p-6 text-center">
+                        <h3 className="text-2xl font-semibold text-gray-800 mb-2">
+                          {book.title}
+                        </h3>
+                        <p className="text-gray-600 mb-4">{book.summary}</p>
+                        <div className="flex flex-col gap-2">
+                          <a href={book.pdfLink} download>
+                            <Button
+                              type="button"
+                              text="Lire un extrait"
+                              className={styles["button-white-border"]}
+                            />
+                          </a>
                           <Button
                             type="button"
                             text="Acheter"
--- src/app/ui/button/button.tsx
+++ src/app/ui/button/button.tsx
@@ -3,19 +3,21 @@ export default function Button({
   text,
   disabled,
   onClick,
+  target,
   className,
 }: {
   type: "submit" | "button";
   text: string;
   disabled?: boolean;
   onClick?: () => void;
+  target?: string;
   className?: string;
 }) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`p-2 w-50 cursor-pointer mx-auto border-2 transition-colors duration-300 ease-in-out ${className}`}
+      className={`p-2 w-50 cursor-pointer rounded-full mx-auto  transition-colors duration-300 ease-in-out ${className}`}
       aria-label={text}
       disabled={disabled}>
       {text}
