390dbb574c90db04ed520f50c461f0a8f65f01b6
Priscillia <selvapriscillia@yahoo.fr>
2025-02-05 21:08:56 +0100

    fix error vercel

--- src/app/components/biographyTrilogy/biographyTrilogy.tsx
+++ src/app/components/biographyTrilogy/biographyTrilogy.tsx
@@ -2,86 +2,85 @@ import Image from "next/image";
 
 export default function BiographyTrilogy() {
   return (
-    <section className="text-justify p-6 bg-blue-color color-text-white mb-6 w-full">
-      <Image
-        src="/books.png"
-        alt="All books"
-        width={200}
-        height={200}
-        className=" w-48 h-48 md:w-48 md:h-64 lg:w-48 lg:h-64"
-      />
-      <h3 className="text-2xl font-bold mb-4">Biographie</h3>
-      <p className="mb-4">
+    <section className="text-justify p-6 bg-blue-color text-white mb-6 w-full">
+      <div className="flex justify-center p-0">
+        <Image
+          src="/books.png"
+          alt="All books"
+          width={400}
+          height={400}
+          className="object-contain w-64 h-64 md:w-80 md:h-96 lg:w-80 lg:h-96 m-0"
+        />
+      </div>
+      <h3 className="text-3xl font-extrabold mb-6 text-center">Biographie</h3>
+      <p className="mb-4 text-lg leading-relaxed">
         Les deux trilogies « Gaïa » et sa suite, « le Nouveau Monde » sont des
-        fictions contemporaines qui n’ont pour seule ambition que de mettre
-        l’homme face à ses responsabilités, à un moment crucial ou la survie de
-        notre planète et de l’ensemble des espèces repose entre ses mains.
+        fictions contemporaines qui n&apos;ont pour seule ambition que de mettre
+        l&apos;homme face à ses responsabilités, à un moment crucial où la
+        survie de notre planète et de l&apos;ensemble des espèces repose entre
+        ses mains.
       </p>
-      <p className="mb-4">
-        « Car si l’homme n’a pas la sagesse de respecter la vie, alors le monde
-        ne risque-t-il pas de continuer sans lui » *
-        <span className="italic">*Théodore Monod</span>
+      <p className="mb-4 text-lg leading-relaxed">
+        « Car si l&apos;homme n&apos;a pas la sagesse de respecter la vie, alors
+        le monde ne risque-t-il pas de continuer sans lui ? »
+        <span className="italic block mt-2">*Théodore Monod</span>
       </p>
-      <p className="mb-4">
+      <p className="mb-4 text-lg leading-relaxed">
         Les trois cycles de la trilogie « Gaïa », que sont « Game Over », « la
         Révolte des animaux » et « le Voyage ultime », sont construits autour de
         la théorie de James Lovelock*, célèbre scientifique britannique, père de
-        l’hypothèse « Gaia », selon laquelle l’ensemble terre-atmosphère possède
-        les caractéristiques essentielles des êtres vivants.
+        l&apos;hypothèse « Gaia », selon laquelle l&apos;ensemble
+        terre-atmosphère possède les caractéristiques essentielles des êtres
+        vivants.
       </p>
-      <p className="mb-4">
+      <p className="mb-4 text-lg leading-relaxed">
         La Terre, gigantesque organisme vivant, dont les écosystèmes naturels
-        sont les organes, dont l’air et l’eau sont les fluides vitaux, et
-        l’Homme, l’une des innombrables cellules au même titre que les autres
-        espèces animales et végétales.
+        sont les organes, dont l&apos;air et l&apos;eau sont les fluides vitaux,
+        et l&apos;Homme, l&apos;une des innombrables cellules au même titre que
+        les autres espèces animales et végétales.
       </p>
-      <p className="mb-4">
+      <p className="mb-4 text-lg leading-relaxed">
         De cette hypothèse fascinante, est née cette trilogie : Trois cycles,
-        trois époques, trois personnalités emblématiques issues d’une même
-        lignée, trois fictions originales dont Gaïa** est le lien, l’épicentre
-        pour une seule quête : l’Harmonie du Monde, toutes les espèces vivantes
-        en totale harmonie dans le respect du pacte originel.
-        <span className="block mt-2 italic">
-          *James Lovelock : Pour illustrer sa théorie — appelée hypothèse Gaïa
-          —, l'écologiste anglais James Lovelock utilise, dès 1970, le nom et
-          l'image de la déesse-mère Gaïa, personnifiant « la Terre comme un être
-          vivant » (titre de son ouvrage fondateur). Selon lui, la Terre est un
-          superorganisme, un système intelligent, s'autorégulant, pour permettre
-          le développement de la Vie, objectif permis au moyen des lois
-          gaïennes.
-        </span>
-        <span className="block mt-2 italic">
-          **Gaïa : Dans la mythologie grecque, Gaïa est la déesse primordiale
-          identifiée à la « déesse mère » et à la mère des Titans. Unie à
-          Ouranos, le dieu du Ciel, elle engendra les six Titans et les six
-          Titanides, puis les Cyclopes, suivi des Hécatonchires (les monstres
-          aux cent bras) et enfin les Géants.
-        </span>
+        trois époques, trois personnalités emblématiques issues d&apos;une même
+        lignée, trois fictions originales dont Gaïa** est le lien,
+        l&apos;épicentre pour une seule quête : l&apos;Harmonie du Monde, toutes
+        les espèces vivantes en totale harmonie dans le respect du pacte
+        originel.
       </p>
-      <p className="mb-4">
+      <span className="block mt-4 italic text-sm">
+        *James Lovelock : Pour illustrer sa théorie — appelée hypothèse Gaïa —,
+        l&apos;écologiste anglais James Lovelock utilise, dès 1970, le nom et
+        l&apos;image de la déesse-mère Gaïa, personnifiant « la Terre comme un
+        être vivant » (titre de son ouvrage fondateur).
+      </span>
+      <span className="block mt-4 italic text-sm mb-6">
+        **Gaïa : Dans la mythologie grecque, Gaïa est la déesse primordiale
+        identifiée à la « déesse mère » et à la mère des Titans.
+      </span>
+      <p className="mb-4 text-lg leading-relaxed">
         Les deux premiers cycles de la trilogie du « Nouveau Monde » : « les
         bicéphales », et « Morpheus ou la révolution poétique » mettent en scène
         le personnage de Népomucène, fils de Gaïa, « Créateur de Monde », déjà
         entrevu dans « le Voyage ultime ». A la recherche de sa mère, Népomucène
         doit affronter les bicéphales, des êtres humains dotés de deux cerveaux,
-        qui ont éradiqué l’imagination et la puissance créatrice du cortex des
-        humains monocéphales, pour les asservir.
+        qui ont éradiqué l&apos;imagination et la puissance créatrice du cortex
+        des humains monocéphales, pour les asservir.
       </p>
-      <p className="mb-4">
+      <p className="mb-4 text-lg leading-relaxed">
         Cet affrontement, ainsi que ses excursions dans le passé des hommes de «
-        l’Ancien Monde », sont des champs d’expérimentations qui n’ont pour
-        d’autre finalité que de transcender la condition humaine de Népomucène.
-        Il doit franchir et réussir ces étapes, pour prendre un jour la place de
-        sa mère, car tel est son destin.
+        l&apos;Ancien Monde », sont des champs d&apos;expérimentations qui
+        n&apos;ont pour d&apos;autre finalité que de transcender la condition
+        humaine de Népomucène. Il doit franchir et réussir ces étapes, pour
+        prendre un jour la place de sa mère, car tel est son destin.
       </p>
-      <p className="mb-4">
-        Il est aidé dans cette quête par Morpheus, un traite à la solde des
+      <p className="mb-4 text-lg leading-relaxed">
+        Il est aidé dans cette quête par Morpheus, un traître à la solde des
         bicéphales qui a pris fait et cause pour Népomucène, en menant sa
         révolution, « une révolution des esprits », et par la Chimère, illustre
-        représentante des créatures de « l’entre-deux-mondes » qui n’existent
-        que grâce à l’imagination des hommes.
+        représentante des créatures de « l&apos;entre-deux-mondes » qui
+        n&apos;existent que grâce à l&apos;imagination des hommes.
       </p>
-      <p className="mb-4">
+      <p className="mb-4 text-lg leading-relaxed">
         Le troisième et dernier cycle de cette trilogie est en cours de
         rédaction pour une publication prévue sur 2026.
       </p>
